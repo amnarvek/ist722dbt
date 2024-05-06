a3a669d3e1232772d637e9703dc4d2933ac318f2
Advait Narvekar <amnarvek@g.syr.edu>
2024-05-06 19:43:42 +0000

    commit 2

--- vision/models/fact_customer_order_per_day.sql
+++ vision/models/fact_customer_order_per_day.sql
@@ -1,38 +1,19 @@
-with 
+with
+    stg_order_details as (select * from {{ source("visionmart", "order_details") }}),
+    stg_customers as (select * from {{ source("visionmart", "customers") }}),
+    stg_dim_date as (select * from {{ source("visionmart", "DateDimension") }}),
+    stg_products as (select * from {{ source("visionmart", "products") }}),
+    stg_orders as (select * from {{ source("visionmart", "orders") }})
 
-stg_order_details as (
-    select * from {{ source('visionmart','order_details')}}
-),
-stg_customers as (
-    select * from {{ source('visionmart','customers')}}
-),
-
-stg_dim_date as (
-    select * from {{ source('visionmart','DateDimension')}}
-),
-stg_products as (
-    select * from {{ source('visionmart','products')}}
-),
-
-stg_orders as (
-    select * from {{ source('visionmart','orders')}}
-)
-
-SELECT 
-{{dbt_utils.generate_surrogate_key(['c.customer_id'])}} as fact_customer_order_per_day_key,
-c.customer_id,
-o.order_date,
-o.order_id,
-sum(p.product_retail_price * od.order_qty) as price
-FROM
-    stg_orders o
-JOIN
-    stg_order_details od ON o.ORDER_ID = od.ORDER_ID
-JOIN 
-    stg_products p on p.product_id = od.product_id
-JOIN
-    stg_customers c on c.customer_id = o.customer_id
-group BY
-o.order_date,
-c.customer_id,
-o.order_id
+select
+    {{ dbt_utils.generate_surrogate_key(["c.customer_id"]) }}
+    as fact_customer_order_per_day_key,
+    c.customer_id,
+    o.order_date,
+    o.order_id,
+    sum(p.product_retail_price * od.order_qty) as price
+from stg_orders o
+join stg_order_details od on o.order_id = od.order_id
+join stg_products p on p.product_id = od.product_id
+join stg_customers c on c.customer_id = o.customer_id
+group by o.order_date, c.customer_id, o.order_id
--- vision/models/fact_overall_sales.sql
+++ vision/models/fact_overall_sales.sql
@@ -1,25 +1,39 @@
-with stg_account_billing as (
-    select * from {{ source('visionflix','account_billing')}}
-),
-stg_plans as (
-    select * from {{ source('visionflix','plans')}}
-),
-stg_dim_date as (
-    select * from {{ source('visionmart','DateDimension')}}
-)
+-- with
+-- stg_account_billing as (
+-- select * from {{ source("visionflix", "account_billing") }}
+-- ),
+-- stg_plans as (select * from {{ source("visionflix", "plans") }}),
+-- stg_dim_date as (select * from {{ source("visionmart", "DateDimension") }})
+-- select
+-- {{ dbt_utils.generate_surrogate_key(["a.ab_id"]) }} as fact_overall_sales_key,
+-- a.ab_account_id as customer_key,
+-- a.ab_plan_id as plan_key,
+-- month,
+-- year,
+-- sum((p.plan_price * p.discount) / 100) as visionflix_total_discount,
+-- sum((p.plan_price - p.discount)) as visionflix_total_amount
+-- from stg_account_billing a
+-- inner join stg_plans p on a.ab_plan_id = p.plan_id
+-- inner join stg_dim_date d on a.ab_date = d.datetime
+-- group by fact_overall_sales_key, customer_key, plan_key, month, year
+-- order by customer_key, month, year
+with
+    stg_order_details as (select * from {{ source("visionmart", "order_details") }}),
+    stg_customers as (select * from {{ source("visionmart", "customers") }}),
+    stg_dim_date as (select * from {{ source("visionmart", "DateDimension") }}),
+    stg_products as (select * from {{ source("visionmart", "products") }}),
+    stg_orders as (select * from {{ source("visionmart", "orders") }})
+
 select
-    {{dbt_utils.generate_surrogate_key(['a.ab_id'])}} as fact_overall_sales_key,
-    a.ab_account_id as customer_key,
-    a.ab_plan_id as plan_key,
-    a.ab_date as date_key,
+    {{ dbt_utils.generate_surrogate_key(["c.customer_id"]) }}
+    as fact_customer_order_per_day_key,
+    c.customer_id as customer_key,
     month,
     year,
-    p.discount,
-    p.plan_price as amount,
-    (p.plan_price*p.discount)/100 as total_discount,
-    (p.plan_price-p.discount) as total_amount
-from
-    stg_account_billing a inner join stg_plans p on a.ab_plan_id = p.plan_id
-    inner join stg_dim_date d on a.ab_date = d.datetime
-order by
-    customer_key
+    o.order_id,
+    sum(p.product_retail_price * od.order_qty) as visionmart_total_amount
+from stg_orders o
+join stg_order_details od on o.order_id = od.order_id
+join stg_products p on p.product_id = od.product_id
+join stg_customers c on c.customer_id = o.customer_id
+group by customer_key, month, year
