47e49136c581ce38bd0f47ddab1daf3f1792a423
Advait Narvekar <amnarvek@g.syr.edu>
2024-05-08 18:39:33 +0000

    Overall Facts

--- vision/models/fact_overall_sales.sql
+++ vision/models/fact_overall_sales.sql
@@ -17,23 +17,24 @@
 -- inner join stg_dim_date d on a.ab_date = d.datetime
 -- group by fact_overall_sales_key, customer_key, plan_key, month, year
 -- order by customer_key, month, year
+
 with
     stg_order_details as (select * from {{ source("visionmart", "order_details") }}),
     stg_customers as (select * from {{ source("visionmart", "customers") }}),
     stg_dim_date as (select * from {{ source("visionmart", "DateDimension") }}),
     stg_products as (select * from {{ source("visionmart", "products") }}),
     stg_orders as (select * from {{ source("visionmart", "orders") }})
-
 select
     {{ dbt_utils.generate_surrogate_key(["c.customer_id"]) }}
     as fact_customer_order_per_day_key,
     c.customer_id as customer_key,
     month,
     year,
-    o.order_id,
     sum(p.product_retail_price * od.order_qty) as visionmart_total_amount
 from stg_orders o
-join stg_order_details od on o.order_id = od.order_id
-join stg_products p on p.product_id = od.product_id
-join stg_customers c on c.customer_id = o.customer_id
+inner join stg_order_details od on o.order_id = od.order_id
+inner join stg_dim_date d on o.order_date = d.date
+inner join stg_products p on p.product_id = od.product_id
+inner join stg_customers c on c.customer_id = o.customer_id
 group by customer_key, month, year
+order by customer_key, month, year
